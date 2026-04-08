ef78bfbe0ef7d7dae6c8cab460fcbd9953e2c341
Fatema <fatematz486@gmail.com>
2026-04-09 00:07:56 +0600

    Added navbar

---
+++ src/app/about/info/page.jsx
@@ -0,0 +1,11 @@
+
+
+const InfoPage = () => {
+    return (
+        <div>
+            <h1>Info page</h1>
+        </div>
+    );
+};
+
+export default InfoPage;
---
+++ src/app/contact/page.jsx
@@ -0,0 +1,11 @@
+
+
+const ContactPage = () => {
+    return (
+        <div>
+            <h1 className="text-4xl">Call Now</h1>
+        </div>
+    );
+};
+
+export default ContactPage;
